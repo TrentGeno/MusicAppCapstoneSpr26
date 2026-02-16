d99f19064fefaf05927779e44967750847a67116
Hyrakan <144388071+Hyrakan@users.noreply.github.com>
2026-02-16 17:08:03 -0600

    Setup work for Auth0 login

---
+++ frontend/auth.js
@@ -0,0 +1,36 @@
+import { createAuth0Client } from '@auth0/auth0-spa-js'
+import axios from 'axios'
+import { useState } from 'react'
+import { createContext } from 'react'
+import { useContext } from 'react'
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+    const [client, setClient] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loginFailed, setLoginFailed] = useState(false);
+    const [user, setUser] = useState(null);
+
+    const value = {
+        client,
+        setClient,
+        isAuthenticated,
+        setIsAuthenticated,
+        loginFailed,
+        setLoginFailed,
+        user,
+        setUser
+    };
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
