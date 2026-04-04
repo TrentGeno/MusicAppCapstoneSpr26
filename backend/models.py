bb70859f045ecf551d43345d42d1a95004a778dd
GabrielAlme <gabealmeemail@gmail.com>
2026-04-04 15:22:19 -0500

    made playlists sort by newest song added and made the cover arts reflect that

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,8 @@ from datetime import datetime
 
 playlist_tracks = db.Table('playlist_tracks',
     db.Column('playlist_id', db.Integer, db.ForeignKey('playlists.playlist_id'), primary_key=True),
-    db.Column('track_id', db.Integer, db.ForeignKey('tracks.track_id'), primary_key=True)
+    db.Column('track_id', db.Integer, db.ForeignKey('tracks.track_id'), primary_key=True),
+    db.Column('added_at', db.DateTime, default=datetime.utcnow)
 )
 
 class User(db.Model):
@@ -42,7 +43,6 @@ class Playlist(db.Model):
     playlist_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(255))
-
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-
     tracks = db.relationship('Track', secondary=playlist_tracks, backref='playlists')
